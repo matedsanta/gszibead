ed66fcafe49d5859493bd13ce116aa17ee4f2501
matedsanta <santa.mate.daniel@gmail.com>
2024-12-17 22:21:23 +0100

    Finished 2-3

--- 2024.12.17/Gyakorlás 2/3-feladat.py
+++ 2024.12.17/Gyakorlás 2/3-feladat.py
@@ -1,16 +1,18 @@
-def legmagasabb(elemek: dict) -> dict:
-    legmagasabb_nev = ""
-    legmagasabb_mag = 0
-    for elem in elemek:
-        if elemek.get(elem) > legmagasabb_mag:
-            legmagasabb_mag = elemek.get(elem)
-            legmagasabb_nev = elem
-    return {legmagasabb_nev: legmagasabb_mag}
-
-
-hegyek = {}
+hegyek = []
+magassagok = []
 for i in range(3):
     nev = input("Írja be a hegy nevét: ")
     magassag = int(input(f"Írja be a {nev} magasságát: "))
-    hegyek.update({nev: magassag})
-print(f"A legmagasabb a {legmagasabb(hegyek)}")
+    hegyek.append(nev)
+    magassagok.append(magassag)
+
+legmagasabb = 0
+legmagasabb_index = 0
+for i in range(len(magassagok)):
+    if magassagok[i] > legmagasabb:
+        legmagasabb = magassagok[i]
+        legmagasabb_index = i
+
+print(
+    f"A legmagasabb hegy a {hegyek[legmagasabb_index]}, a magassága {magassagok[legmagasabb_index]}m"
+)
