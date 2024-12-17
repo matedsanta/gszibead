475dcc8997ce37a25a9f8c09246c33d655c923f3
matedsanta <santa.mate.daniel@gmail.com>
2024-12-17 22:23:49 +0100

    Extracted method

--- 2024.12.17/Gyakorlás 2/3-feladat.py
+++ 2024.12.17/Gyakorlás 2/3-feladat.py
@@ -1,17 +1,24 @@
 hegyek = []
 magassagok = []
+
 for i in range(3):
     nev = input("Írja be a hegy nevét: ")
     magassag = int(input(f"Írja be a {nev} magasságát: "))
     hegyek.append(nev)
     magassagok.append(magassag)
 
-legmagasabb = 0
-legmagasabb_index = 0
-for i in range(len(magassagok)):
-    if magassagok[i] > legmagasabb:
-        legmagasabb = magassagok[i]
-        legmagasabb_index = i
+
+def legmagasabb(magassagok):
+    legmagasabb = 0
+    legmagasabb_index = 0
+    for i in range(len(magassagok)):
+        if magassagok[i] > legmagasabb:
+            legmagasabb = magassagok[i]
+            legmagasabb_index = i
+    return legmagasabb_index
+
+
+legmagasabb_index = legmagasabb(magassagok)
 
 print(
     f"A legmagasabb hegy a {hegyek[legmagasabb_index]}, a magassága {magassagok[legmagasabb_index]}m"
